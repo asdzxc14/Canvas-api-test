02378bbbb02f5b60d90b649c7332a04c2d5255a0
asdzxc14 <xuyuanchao95@163.com>
2017-02-21 11:00:58 +0800

    添加alpha值

--- out/main.js
+++ out/main.js
@@ -19,6 +19,7 @@ window.onload = function () {
     tf1.x = 40;
     tf1.y = 50;
     tf1.size = 20;
+    tf1.alpha = 1;
     stage.addChild(tf1);
     var tf2 = new TextField();
     tf2.text = "World";
@@ -33,12 +34,14 @@ window.onload = function () {
     bitmap.width = 300;
     bitmap.height = 300;
     bitmap.y = 70;
+    bitmap.alpha = 0.8;
     stage.addChild(bitmap);
 };
 var DisplayObject = (function () {
     function DisplayObject() {
         this.x = 0;
         this.y = 0;
+        this.alpha = 1;
     }
     DisplayObject.prototype.draw = function (context2D) {
     };
@@ -69,6 +72,7 @@ var TextField = (function (_super) {
         return _this;
     }
     TextField.prototype.draw = function (context2D) {
+        context2D.globalAlpha = this.alpha;
         context2D.font = this.size + "px" + " " + this.font;
         context2D.fillText(this.text, this.x, this.y);
     };
@@ -80,8 +84,101 @@ var Bitmap = (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Bitmap.prototype.draw = function (context2D) {
+        context2D.globalAlpha = this.alpha;
         context2D.drawImage(this.image, this.x, this.y, this.width, this.height);
     };
     return Bitmap;
 }(DisplayObject));
+var math;
+(function (math) {
+    var Point = (function () {
+        function Point(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        return Point;
+    }());
+    math.Point = Point;
+    function pointAppendMatrix(point, m) {
+        var x = m.a * point.x + m.c * point.y + m.tx;
+        var y = m.b * point.x + m.d * point.y + m.ty;
+        return new Point(x, y);
+    }
+    math.pointAppendMatrix = pointAppendMatrix;
+    /**
+     * 使用伴随矩阵法求逆矩阵
+     * http://wenku.baidu.com/view/b0a9fed8ce2f0066f53322a9
+     */
+    function invertMatrix(m) {
+        var a = m.a;
+        var b = m.b;
+        var c = m.c;
+        var d = m.d;
+        var tx = m.tx;
+        var ty = m.ty;
+        var determinant = a * d - b * c;
+        var result = new Matrix(1, 0, 0, 1, 0, 0);
+        if (determinant == 0) {
+            throw new Error("no invert");
+        }
+        determinant = 1 / determinant;
+        var k = result.a = d * determinant;
+        b = result.b = -b * determinant;
+        c = result.c = -c * determinant;
+        d = result.d = a * determinant;
+        result.tx = -(k * tx + c * ty);
+        result.ty = -(b * tx + d * ty);
+        return result;
+    }
+    math.invertMatrix = invertMatrix;
+    function matrixAppendMatrix(m1, m2) {
+        var result = new Matrix();
+        result.a = m1.a * m2.a + m1.b * m2.c;
+        result.b = m1.a * m2.b + m1.b * m2.d;
+        result.c = m2.a * m1.c + m2.c * m1.d;
+        result.d = m2.b * m1.c + m1.d * m2.d;
+        result.tx = m2.a * m1.tx + m2.c * m1.ty + m2.tx;
+        result.ty = m2.b * m1.tx + m2.d * m1.ty + m2.ty;
+        return result;
+    }
+    math.matrixAppendMatrix = matrixAppendMatrix;
+    var PI = Math.PI;
+    var HalfPI = PI / 2;
+    var PacPI = PI + HalfPI;
+    var TwoPI = PI * 2;
+    var DEG_TO_RAD = Math.PI / 180;
+    var Matrix = (function () {
+        function Matrix(a, b, c, d, tx, ty) {
+            if (a === void 0) { a = 1; }
+            if (b === void 0) { b = 0; }
+            if (c === void 0) { c = 0; }
+            if (d === void 0) { d = 1; }
+            if (tx === void 0) { tx = 0; }
+            if (ty === void 0) { ty = 0; }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.tx = tx;
+            this.ty = ty;
+        }
+        Matrix.prototype.toString = function () {
+            return "(a=" + this.a + ", b=" + this.b + ", c=" + this.c + ", d=" + this.d + ", tx=" + this.tx + ", ty=" + this.ty + ")";
+        };
+        Matrix.prototype.updateFromDisplayObject = function (x, y, scaleX, scaleY, rotation) {
+            this.tx = x;
+            this.ty = y;
+            var skewX, skewY;
+            skewX = skewY = rotation / 180 * Math.PI;
+            var u = Math.cos(skewX);
+            var v = Math.sin(skewX);
+            this.a = Math.cos(skewY) * scaleX;
+            this.b = Math.sin(skewY) * scaleX;
+            this.c = -v * scaleY;
+            this.d = u * scaleY;
+        };
+        return Matrix;
+    }());
+    math.Matrix = Matrix;
+})(math || (math = {}));
 //# sourceMappingURL=main.js.map
